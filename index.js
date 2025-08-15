30c9081f383a196c16657d4107238111a6f8c44c
Rabia Khan <rabiakhan@Rabias-MacBook-Pro.local>
2025-08-15 12:12:08 -0400

    Finiliazed portfolio

--- index.js
+++ index.js
@@ -98,3 +98,16 @@ document.querySelectorAll('.carousel').forEach(carousel => {
     updateCarousel();
   });
 });
+const tabButtons = document.querySelectorAll('#certifications .tab-btn');
+const tabContents = document.querySelectorAll('#certifications .tab-content');
+
+tabButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    tabButtons.forEach(b => b.classList.remove('active'));
+    tabContents.forEach(c => c.classList.remove('active'));
+
+    btn.classList.add('active');
+    document.getElementById(btn.dataset.tab).classList.add('active');
+  });
+});
+
